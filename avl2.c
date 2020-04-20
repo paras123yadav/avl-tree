18b3183f84f564f23ede225d7cdedadda0b6844b
paras123yadav <54791137+paras123yadav@users.noreply.github.com>
2020-04-20 20:13:39 +0530

    Update avl2.c

--- avl2.c
+++ avl2.c
@@ -3,8 +3,8 @@
 #define max(a,b) a>b?a:b
 typedef struct node{
     int data;
-    struct node* lchild;
-    struct node* rchild;
+    struct node* left;
+    struct node* right;
 }node;
 node* root=NULL;
 int key=1;
@@ -14,8 +14,8 @@ int height(node* root)// find the height of the tree
 {
     if (!root)
     return 0;
-    int hl=height(root->lchild);
-    int hr=height(root->rchild);
+    int hl=height(root->left);
+    int hr=height(root->right);
     h_max=max(hl,hr);
     return (1+h_max);
 }
@@ -23,14 +23,14 @@ node* t1=NULL,*t2=NULL;
 node* unbalanced(node* root)// return unbalanced node's address else null
 {
     if (!root) return NULL;
-    int hl=height(root->lchild);
-    int hr=height(root->rchild);
+    int hl=height(root->left);
+    int hr=height(root->right);
     if (hl-hr>=2)
     return root;
     if (hl-hr<=-2)
     return root;
-    t1=unbalanced(root->lchild);
-    t2=unbalanced(root->rchild);
+    t1=unbalanced(root->left);
+    t2=unbalanced(root->right);
     if (t1==NULL && t2==NULL)
     return NULL;
     else if (t1==NULL)
@@ -44,13 +44,13 @@ node* insert(node* t,int key)// inserts key inside the trey by recursion
     {
         node* temp=(node*)malloc(sizeof(node));
         temp->data=key;
-        temp->lchild=temp->rchild=NULL;
+        temp->left=temp->right=NULL;
         return temp;
     }
     if (t->data>key)
-    t->lchild=insert(t->lchild,key);
+    t->lchild=insert(t->left,key);
     if (t->data<key)
-    t->rchild=insert(t->rchild,key);
+    t->rchild=insert(t->right,key);
     return t;
 }
 //type of rotaion of the tree 
@@ -61,7 +61,7 @@ int rotation(int key)//assuming left = 1 right = 2
     {
         if (temp->data<key)
         {
-            temp=temp->rchild;
+            temp=temp->right;
             r[i]=2;
             printf("R");
         }
@@ -79,11 +79,11 @@ void balance(node* root,int rv)// i hve created this only for this case
 {
     if (rv==22)//RR rotation
     {
-        node* b=root->rchild;
-        node* c=b->rchild;
-        root->rchild=b->lchild;
-        b->rchild=c;
-        b->lchild=root;
+        node* b=root->right;
+        node* c=b->right;
+        root->right=b->left;
+        b->right=c;
+        b->left=root;
     }
 }
 //this creates the A.V.L tree recursively 
